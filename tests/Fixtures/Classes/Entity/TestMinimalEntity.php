99a3efc1d6f81105d6f75f7b87ebe218592b0e8c
weeger <romain.weeger@gmail.com>
2025-02-08 18:40:44 +0100

    Test runs

--- tests/Fixtures/Classes/Entity/TestMinimalEntity.php
+++ tests/Fixtures/Classes/Entity/TestMinimalEntity.php
@@ -4,14 +4,24 @@ namespace Wexample\SymfonyPseudocode\Tests\Fixtures\Classes\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
 
+/**
+ * A Symfony entity that only contains an id in a flat notation
+ */
 #[ORM\Entity]
 class TestMinimalEntity
 {
+    /**
+     * The identifier of the entity.
+     */
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
     private ?int $id = null;
 
+    /**
+     * Returns the unique id of the entity instance.
+     * @return int|null
+     */
     public function getId(): ?int
     {
         return $this->id;
