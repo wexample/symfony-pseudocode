3d3dbc96a89f9bd1af5f5922558c5e4bdf31b7b5
weeger <romain.weeger@gmail.com>
2025-02-08 12:54:42 +0100

    Pseudocode find entity

--- src/Command/PseudocodeCommand.php
+++ src/Command/PseudocodeCommand.php
@@ -5,21 +5,42 @@ namespace Wexample\SymfonyPseudocode\Command;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Finder\Finder;
+use Symfony\Component\HttpKernel\KernelInterface;
+use Wexample\SymfonyHelpers\Service\BundleService;
 
 class PseudocodeCommand extends AbstractPseudocodeGenerateCommand
 {
+    public function __construct(
+        protected BundleService $bundleService,
+        protected KernelInterface $container
+    )
+    {
+        parent::__construct($bundleService);
+    }
+
     protected function execute(
         InputInterface $input,
         OutputInterface $output
     ): int
     {
-        $pseudocodeDir = $input->getArgument('pseudocode-dir');
+        $projectDir = $this->container->getProjectDir();
+        $entityDir = $projectDir . '/src/Entity';
 
-        // TODO: Generate pseudocode from the symfony project.
-        // - Explore the Entity directory to find entities
-        // - Fill the outputDir with generated pseudocode based on found files.
+        $finder = new Finder();
+        $finder->files()
+            ->in($entityDir)
+            ->name('*.php');
 
-        $output->writeln('TODO: Implement pseudocode to Symfony project conversion');
+        if (!$finder->hasResults()) {
+            $output->writeln('No entity found in ' . $entityDir);
+            return Command::SUCCESS;
+        }
+
+        foreach ($finder as $file) {
+            $relativePath = $file->getRelativePathname();
+            $output->writeln('Entity found: ' . $relativePath);
+        }
 
         return Command::SUCCESS;
     }
