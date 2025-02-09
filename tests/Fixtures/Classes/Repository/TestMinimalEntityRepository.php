c6a5a1a85c6958f1162dc1d4cc82ea63a29131df
weeger <romain.weeger@gmail.com>
2025-02-09 19:03:43 +0100

    Symfony pseudocode basic test pass

--- tests/Fixtures/Classes/Repository/TestMinimalEntityRepository.php
+++ tests/Fixtures/Classes/Repository/TestMinimalEntityRepository.php
@@ -6,8 +6,16 @@ use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
 use Wexample\SymfonyPseudocode\Tests\Fixtures\Entity\TestMinimalEntity;
 
+/**
+ * A basic Symfony repository
+ */
 class TestMinimalEntityRepository extends ServiceEntityRepository
 {
+    /**
+     * Class initialization.
+     *
+     * @param ManagerRegistry $registry A given service.
+     */
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, TestMinimalEntity::class);
