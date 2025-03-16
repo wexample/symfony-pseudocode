d5281c2647982ed4b213d15067b14c3fd8e70abf
weeger <romain.weeger@gmail.com>
2025-03-16 11:40:21 +0100

    Non tested directory processing

--- src/Command/PseudocodeCommand.php
+++ src/Command/PseudocodeCommand.php
@@ -3,7 +3,9 @@
 namespace Wexample\SymfonyPseudocode\Command;
 
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\HttpKernel\KernelInterface;
 use Wexample\SymfonyHelpers\Service\BundleService;
@@ -21,16 +23,65 @@ class PseudocodeCommand extends AbstractPseudocodeGenerateCommand
         parent::__construct($bundleService);
         $this->pseudocodeService = new PseudocodeService($kernel);
     }
+    
+    protected function configure(): void
+    {
+        parent::configure();
+        
+        $this
+            ->addArgument(
+                'source-path',
+                InputArgument::OPTIONAL,
+                'Specific file or directory to process (relative to project root)',
+                'src'
+            )
+            ->addOption(
+                'recursive',
+                'r',
+                InputOption::VALUE_NONE,
+                'Process directories recursively'
+            );
+    }
 
     protected function execute(
         InputInterface $input,
         OutputInterface $output
     ): int
     {
-        $this->pseudocodeService->process(
-            $this->kernel->getProjectDir() . '/' . $input->getArgument('pseudocode-dir') . '/'
+        $projectDir = $this->kernel->getProjectDir();
+        $sourcePath = $input->getArgument('source-path');
+        $pseudocodeDir = $input->getArgument('pseudocode-dir');
+        $recursive = $input->getOption('recursive');
+        
+        // Make sure paths are absolute
+        $sourcePath = $this->resolvePath($projectDir, $sourcePath);
+        $pseudocodeDir = $this->resolvePath($projectDir, $pseudocodeDir);
+        
+        $output->writeln(sprintf('Processing source: %s', $sourcePath));
+        $output->writeln(sprintf('Output directory: %s', $pseudocodeDir));
+        
+        $files = $this->pseudocodeService->process(
+            $pseudocodeDir,
+            $sourcePath,
+            $recursive
         );
+        
+        $output->writeln(sprintf('Processed %d files', count($files)));
 
         return Command::SUCCESS;
+    }
+    
+    /**
+     * Resolve a path to an absolute path
+     */
+    private function resolvePath(string $baseDir, string $path): string
+    {
+        // If path is already absolute, return it
+        if (strpos($path, '/') === 0) {
+            return $path;
+        }
+        
+        // Otherwise, make it absolute
+        return $baseDir . '/' . $path;
     }
 }
--- src/Service/PseudocodeService.php
+++ src/Service/PseudocodeService.php
@@ -2,6 +2,7 @@
 
 namespace Wexample\SymfonyPseudocode\Service;
 
+use Symfony\Component\Finder\Finder;
 use Symfony\Component\HttpKernel\KernelInterface;
 use Wexample\SymfonyPseudocode\Generator\SymfonyCodeGenerator;
 use Wexample\SymfonyPseudocode\Generator\SymfonyPseudocodeGenerator;
@@ -16,13 +17,16 @@ class PseudocodeService
     protected RepositoryProcessor $repositoryProcessor;
     /** @var AbstractProcessor[] */
     protected $processors = [];
+    
+    protected SymfonyPseudocodeGenerator $pseudocodeGenerator;
+    protected SymfonyCodeGenerator $codeGenerator;
 
     public function __construct(
         protected KernelInterface $kernel,
     )
     {
-        $pseudocodeGenerator = new SymfonyPseudocodeGenerator();
-        $codeGenerator = new SymfonyCodeGenerator();
+        $this->pseudocodeGenerator = new SymfonyPseudocodeGenerator();
+        $this->codeGenerator = new SymfonyCodeGenerator();
 
         $processors = [
             EntityProcessor::class,
@@ -31,32 +35,126 @@ class PseudocodeService
 
         foreach ($processors as $processor) {
             $this->processors[$processor] = new $processor(
-                $pseudocodeGenerator,
-                $codeGenerator,
+                $this->pseudocodeGenerator,
+                $this->codeGenerator,
             );
         }
     }
 
     /**
      * @param string $pseudocodeDir Directory where to generate pseudocode
-     * @param string|null $codeDir Optional directory containing the source code, defaults to kernel project dir
-     * @return string[]
+     * @param string $sourcePath Path to a specific file or directory to process
+     * @param bool $recursive Process directories recursively
+     * @return string[] List of processed files
      */
     public function process(
         string $pseudocodeDir,
-        ?string $codeDir = null
+        string $sourcePath,
+        bool $recursive = false
     ): array
     {
-        $codeDir = $codeDir ?? $this->kernel->getProjectDir() . '/src';
-
+        // Ensure pseudocode directory ends with a slash
+        if (!str_ends_with($pseudocodeDir, '/')) {
+            $pseudocodeDir .= '/';
+        }
+        
         $files = [];
-        foreach ($this->processors as $processor) {
-            $files = array_merge($processor->process(
-                $codeDir,
-                $pseudocodeDir
-            ), $files);
+        
+        // If source path is a file, process it directly
+        if (is_file($sourcePath)) {
+            return $this->processFile($sourcePath, $pseudocodeDir);
+        }
+        
+        // If source path is a directory, process it with processors
+        if (is_dir($sourcePath)) {
+            // Process with each processor
+            foreach ($this->processors as $processor) {
+                $processorFiles = $this->processDirectory(
+                    $sourcePath,
+                    $pseudocodeDir,
+                    $processor,
+                    $recursive
+                );
+                $files = array_merge($files, $processorFiles);
+            }
         }
 
+        return $files;
+    }
+    
+    /**
+     * Process a single file
+     * 
+     * @param string $filePath Path to the file
+     * @param string $pseudocodeDir Directory where to generate pseudocode
+     * @return string[] List containing the processed file
+     */
+    protected function processFile(string $filePath, string $pseudocodeDir): array
+    {
+        // Get the project root directory to determine relative paths
+        $projectDir = $this->kernel->getProjectDir();
+        
+        // Make sure the file path is absolute
+        if (!str_starts_with($filePath, '/')) {
+            $filePath = $projectDir . '/' . $filePath;
+        }
+        
+        // Only process PHP files
+        if (!str_ends_with($filePath, '.php')) {
+            return [];
+        }
+        
+        // Generate pseudocode for the file
+        $outputFile = $this->pseudocodeGenerator->generateFromFileAndSave(
+            new \SplFileInfo($filePath),
+            $projectDir . '/',
+            $pseudocodeDir
+        );
+        
+        return [$outputFile];
+    }
+    
+    /**
+     * Process a directory using a specific processor
+     * 
+     * @param string $sourceDir Directory to process
+     * @param string $pseudocodeDir Directory where to generate pseudocode
+     * @param AbstractProcessor $processor Processor to use
+     * @param bool $recursive Process directories recursively
+     * @return string[] List of processed files
+     */
+    protected function processDirectory(
+        string $sourceDir,
+        string $pseudocodeDir,
+        AbstractProcessor $processor,
+        bool $recursive = false
+    ): array
+    {
+        // Create a finder to locate files
+        $finder = new Finder();
+        $finder->files()
+            ->in($sourceDir)
+            ->name('*.php');
+        
+        // Set recursion option
+        if (!$recursive) {
+            $finder->depth('== 0');
+        }
+        
+        $files = [];
+        foreach ($finder as $file) {
+            // Generate pseudocode for each file
+            $outputFile = $this->pseudocodeGenerator->generateFromFileAndSave(
+                $file,
+                $this->kernel->getProjectDir() . '/',
+                $pseudocodeDir
+            );
+            
+            if ($outputFile) {
+                $files[] = $outputFile;
+            }
+        }
+        
         return $files;
     }
 }
